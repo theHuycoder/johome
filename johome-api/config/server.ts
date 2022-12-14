1ac79ff9b138f6e856116a2cbc2a75447d7f3660
huydo <thuan@huydos-MacBook-Air.local>
2022-12-15 02:09:53 +0700

    fix

--- johome-api/config/server.ts
+++ johome-api/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "http://johome.site/strapi/",
+  url: "https://johome.site/strapi/",
   app: {
     keys: env.array("APP_KEYS"),
   },
