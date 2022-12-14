2d12c2cbef8cea82435389ae194f31b2bdca4a96
huydo <thuan@huydos-MacBook-Air.local>
2022-12-15 00:27:07 +0700

    fix

--- johome-api/config/middlewares.ts
+++ johome-api/config/middlewares.ts
@@ -1,6 +1,27 @@
 export default [
   "strapi::errors",
-  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "res.cloudinary.com", // cloudinary images
+            "lh3.googleusercontent.com", // google avatars
+            "platform-lookaside.fbsbx.com", // facebook avatars
+            "dl.airtable.com", // strapi marketplace
+          ],
+          "media-src": ["'self'", "data:", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   "strapi::poweredBy",
   {
     name: "strapi::cors",
